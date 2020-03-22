247a2ce8adeb9d7eef89a671c31fdf4ad1f5fd73
sharjeel <leo8545@gmail.com>
2020-03-22 15:32:31 +0500

    Added default timezone & functions for form

--- functions.php
+++ functions.php
@@ -1,6 +1,11 @@
 <?php
+/**
+ * Globaly accessible functions
+ */
 
 use OVS\Utils\Session;
+
+date_default_timezone_set("Asia/Karachi");
 
 function is_user_logged_in() : bool {
 	return Session::get("logged_in") ? true : false;
@@ -8,4 +13,14 @@ function is_user_logged_in() : bool {
 
 function is_admin() : bool {
 	return Session::get("logged_in_as") === "admin" ? true : false;
+}
+
+function is_valid_user_role( string $role ) {
+	$allowed_roles = ["admin", "customer", "dealer"];
+	if(in_array($role, $allowed_roles)) return true;
+	return false;
+}
+
+function checked( $checked, $current ) {
+	return (( isset($checked) && $checked === $current ) ? true : false);
 }
